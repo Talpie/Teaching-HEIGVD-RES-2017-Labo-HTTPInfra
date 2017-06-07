abef96aefc7c21f05ed09ff64ecc3696879c3bda
Tony Clavien <tony.clavien@heig-vd.ch>
2017-06-07 21:46:24 +0200

    step 4 done

--- docker-images/apache-php-image/content/index.php
+++ docker-images/apache-php-image/content/index.php
@@ -1,2 +1,28 @@
+<html>
+<body>
+<h1><span> A beer please </span></h1>
+<script src="jquery.js"></script>
+<script>
+$(function() {
+	console.log("loading beers");
+	
+	function loadBeer() {
+	$.getJSON("/api/students/", function(beers) {
+			console.log(beers);
+			var message = "no beer :(";
+			if(beers.length > 0) {
+				message = beers[0].name + " " + beers[0].type;
+			}
+			console.log(message);
+			$("span").text(message);
+		});
+	};
+	
+	loadBeer();
+	setInterval(loadBeer, 2000);
+});</script>
+</body>
+</html>
 <?php
+
 phpinfo();
